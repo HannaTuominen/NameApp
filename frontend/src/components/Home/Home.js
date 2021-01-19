7ec65ec90c2e51162e783d13bb3fd95d384edfd0
Hanna Tuominen <hanna.h.tuominen@tuni.fi>
2021-01-19 14:07:37 +0200

    Add a github link to the source code

--- frontend/src/components/Home/Home.js
+++ frontend/src/components/Home/Home.js
@@ -4,9 +4,10 @@ const Home = props => (
   <div>
     <div>
       <h1>Welcome to the home of Hanna's Name App</h1>
-      <h4>This site has been made with React frontend and it uses SpringBoot backend with a h2 database. <br/>
+      <p>This site has been made with React frontend and it uses SpringBoot backend with a h2 database. <br/>
         The site has routing for the home screen and the names which displays a list of names that can be ordered and searched by name.
-      </h4>
+      </p>
+      <p>You can check out the code here: <a href={"https://github.com/HannaTuominen/NameApp"}>GitHub</a></p>
     </div>
   </div>
 );
